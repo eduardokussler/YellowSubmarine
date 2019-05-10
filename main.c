e8d0407fd6d6b339963abd6bbcad9dfdea4c3a67
gabrielcoutod <50423926+gabrielcoutod@users.noreply.github.com>
2019-05-10 13:49:09 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,11 @@
 #define LINHA2 25// era 20
 #define COLUNA1 1// era 10
 #define COLUNA2 80// era 70
+#define ALTURASUBMARINO 3
+#define COMPRIMENTOSUBMARINO 11
+#define DIREITA 1
+#define ESQUERDA 0
+
 // to fingindo q sei o q to fazendo
 // parece que já tem o tipo COORDENADA na conio
 typedef struct submarino {
@@ -153,6 +158,122 @@ void le_tecla_imprime2 (){
     clrscr();
 }
 
+void imprime_submarino(SUBMARINO submarino) {
+    // 14 <-> YELLOW
+    textcolor (YELLOW);// troca a cor do texto para amarelo
+    if (submarino.orientacao) {// eh diferente de 0 logo direita
+        cputsxy(submarino.posicao.X+5,submarino.posicao.Y-2,"_");
+        cputsxy(submarino.posicao.X+1,submarino.posicao.Y-1,"___| |___");
+        cputsxy(submarino.posicao.X,submarino.posicao.Y,">\\________)");
+
+    } else {// eh zero logo esquerda
+        cputsxy(submarino.posicao.X+5,submarino.posicao.Y-2,"_");
+        cputsxy(submarino.posicao.X+1,submarino.posicao.Y-1,"___| |___");
+        cputsxy(submarino.posicao.X,submarino.posicao.Y,"(________/<");
+    }
+    textcolor(WHITE);// troca a cor do texto para branco, volta a normal
+
+}
+
+void apaga_submarino(SUBMARINO submarino) {
+    if (submarino.orientacao) {// eh diferente de 0 logo direita
+        cputsxy(submarino.posicao.X+5,submarino.posicao.Y-2," ");
+        cputsxy(submarino.posicao.X+1,submarino.posicao.Y-1,"         ");
+        cputsxy(submarino.posicao.X,submarino.posicao.Y,"           ");
+
+    } else {// eh zero logo esquerda
+        cputsxy(submarino.posicao.X+5,submarino.posicao.Y-2," ");
+        cputsxy(submarino.posicao.X+1,submarino.posicao.Y-1,"         ");
+        cputsxy(submarino.posicao.X,submarino.posicao.Y,"           ");
+    }
+
+}
+
+
+// funcao que move o submarino(porra isso eh meio obvio)(mas ela faz o submarino pular posicoes,executando fica claro)
+// acho q pular para cima e baixo esta certo mas para os lados incorreto, mas achei meio estranho mover uma so posicao para direita ou esquerda
+// pois a tela eh muito pequena para andar tao rapido assim
+// talvez a melhor solucao seja mover entre 1 e 11
+void move_sub(SUBMARINO *submarino){// deixei ainda com dois switch por que vai ter 3 comandos esc espaco e setas
+    // passei por ponteiro para alterar no da funcao principal
+
+    // quando apertar <- ou -> se estiver virado para o lado oposto ele apenas virara para o lado certo se 
+    // ja estiver virada para o lado certo ele "anda", fiz isso pq achei meio roubado virar e andar num movimento so
+    // facil de tirar
+    // obs: n foi proposital mas do jeito que esta fica impossivel caso esteja na borda esquerda virado para a esquerda
+    //      e vira para a direita nao ha como virar novamente para a esquerda
+    char a;
+    int sair = 0;
+    imprime_submarino(*submarino);
+    do {
+        Sleep(50);
+        if (kbhit()) {
+            //fflush(stdin);
+            a = getch();
+            switch(a) {
+                case -32:
+                    a = getch();
+                    switch(a) {// deixei as funcoes de apagar e imprimir dentro dos ifs para n ficar imprimindo e apagando quando desnecessario
+                        case 72:
+                            if (submarino->posicao.Y-ALTURASUBMARINO>LINHA1) {
+                                apaga_submarino(*submarino);
+                                submarino->posicao.Y -=ALTURASUBMARINO;
+                                imprime_submarino(*submarino);
+                            }
+                            break;
+                        case 80:
+                            if (submarino->posicao.Y+ALTURASUBMARINO<LINHA2) {
+                                apaga_submarino(*submarino);
+                                submarino->posicao.Y +=ALTURASUBMARINO;
+                                imprime_submarino(*submarino);
+                            }
+                            //y +=1;
+                            break;
+                        case 77:
+                            // se deseja pular if (submarino->posicao.X+2*COMPRIMENTOSUBMARINO<COLUNA2) {
+                            // se n deseja pular if (submarino->posicao.X+COMPRIMENTOSUBMARINO+1<COLUNA2) {
+                            if (submarino->posicao.X+2*COMPRIMENTOSUBMARINO<COLUNA2) {
+                                // 2 vezes pois precisa verificar se a ponto (11) cabe na proxima posicao
+                                // logo precisa verificar 22
+                                apaga_submarino(*submarino);
+                                if (submarino->orientacao==DIREITA) {
+                                    submarino->posicao.X +=COMPRIMENTOSUBMARINO;
+                                    // se n deseja pular submarino->posicao.X +=1;
+                                    //submarino->posicao.X +=COMPRIMENTOSUBMARINO; se deseja pular
+                                }else {
+                                    submarino->orientacao = DIREITA;
+                                }    
+                                imprime_submarino(*submarino);
+                            }
+                            //x +=1;
+                            break;
+                        case 75:
+                            // se deseja pular if (submarino->posicao.X-COMPRIMENTOSUBMARINO>COLUNA1) {    
+                            // se n deseja pular if (submarino->posicao.X-1>COLUNA1) {  
+                            if (submarino->posicao.X-COMPRIMENTOSUBMARINO>COLUNA1) {                              
+                                apaga_submarino(*submarino);
+                                if (submarino->orientacao==ESQUERDA) {
+                                    submarino->posicao.X -=COMPRIMENTOSUBMARINO;
+                                    // se n deseja pular submarino->posicao.X -=1;
+                                    // submarino->posicao.X -=COMPRIMENTOSUBMARINO; se deseja pular
+                                } else {
+                                    submarino->orientacao = ESQUERDA;
+                                }                                
+                                imprime_submarino(*submarino);
+                            }
+                            //x -=1;
+                            break;
+                    }
+                    break;
+                case 27:
+                    sair  = 1;
+                    break;
+            }
+        }
+    } while(sair==0 && submarino->vidas>0);
+    clrscr();
+}
+
 void imprime_moldura() {
     int x,y;
 
@@ -175,9 +296,10 @@ int main() {
     // tem 30 segundos de oxigenio mas se o cada laço demora
     // meio segundo seria botar 60 de oxigenio e decrementar
     // 1 por laco
-    SUBMARINO sub = {{40,3},1,3,30,0,0};
+    SUBMARINO sub = {{2,4},DIREITA,3,30,0,0};
     //char leitura ;
-   imprime_moldura();
-   //le_tecla_imprime2();
-
+    imprime_moldura();
+    //le_tecla_imprime2();
+    move_sub(&sub);
+    return 0;
 }
